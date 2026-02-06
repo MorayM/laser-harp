8f2910cb2854e3aec15c9c6bd676c89b1dd5ff00
Moray Macdonald <moray.macdonald1@gmail.com>
2026-02-06 09:18:19 +0000

    feat: Setup mode menu structure

--- harp/harp.ino
+++ harp/harp.ino
@@ -12,7 +12,6 @@ constexpr uint8_t LCD_BACKLIGHT_YELLOW = LCD_BACKLIGHT_RED | LCD_BACKLIGHT_GREEN
 constexpr uint8_t LCD_BACKLIGHT_CYAN = LCD_BACKLIGHT_GREEN | LCD_BACKLIGHT_BLUE;
 constexpr uint8_t LCD_BACKLIGHT_MAGENTA = LCD_BACKLIGHT_RED | LCD_BACKLIGHT_BLUE;
 constexpr uint8_t LCD_BACKLIGHT_WHITE = LCD_BACKLIGHT_RED | LCD_BACKLIGHT_GREEN | LCD_BACKLIGHT_BLUE;
-
 constexpr uint8_t HARP_MODE_SETUP = 0;
 constexpr uint8_t HARP_MODE_PLAY = 1;
 
@@ -24,10 +23,32 @@ uint8_t tomStringNotes[SENSOR_COUNT] = {57, 58, 59, 60, 62, 64, 65, 66, 67, 69};
 //                                      C4  G4  C5  F5  G5  Ab5 Bb5 C5  Db5 Eb5
 uint8_t ox7StringNotes[SENSOR_COUNT] = {60, 67, 72, 77, 79, 80, 82, 84, 85, 87};
 
+// Menu
+constexpr uint8_t MENU_COUNT = 4;
+constexpr uint8_t MENU_STRING_MODE = 0;
+constexpr uint8_t MENU_MIDI_CHANNEL = 1;
+constexpr uint8_t MENU_MIDI_VELOCITY = 2;
+constexpr uint8_t MENU_PLAY = 3;
+
+const char *menuLabels[MENU_COUNT] = {
+    "String mode",
+    "MIDI channel",
+    "MIDI velocity",
+    ">> Play"};
+
+constexpr uint8_t STRING_MODE_COUNT = 2;
+const char *stringModeLabels[STRING_MODE_COUNT] = {"Tom", "Ox7"};
+uint8_t *stringModeNotes[STRING_MODE_COUNT] = {tomStringNotes, ox7StringNotes};
+
 // State
 uint8_t harpMode = HARP_MODE_SETUP;
+uint8_t menuIndex = 0;
+uint8_t stringMode = 0;
+uint8_t *stringNotes = tomStringNotes;
 uint8_t midiChannel = 0;
 uint8_t midiVelocity = 100;
+uint8_t lastButtons = 0;
+bool lcdDirty = true;
 
 // -------------------------
 // MIDI helpers
@@ -54,6 +75,162 @@ void midiNoteOff(uint8_t ch, uint8_t note)
   midiWriteByte(0);
 }
 
+// -------------------------
+// LCD helpers
+// -------------------------
+
+// Draw the setup menu for the current menuIndex
+void drawMenu()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(menuLabels[menuIndex]);
+
+  lcd.setCursor(0, 1);
+  switch (menuIndex)
+  {
+  case MENU_STRING_MODE:
+    lcd.print("< ");
+    lcd.print(stringModeLabels[stringMode]);
+    lcd.setCursor(15, 1);
+    lcd.print(">");
+    break;
+  case MENU_MIDI_CHANNEL:
+    lcd.print("< ");
+    lcd.print(midiChannel + 1); // display 1-16
+    lcd.setCursor(15, 1);
+    lcd.print(">");
+    break;
+  case MENU_MIDI_VELOCITY:
+    lcd.print("< ");
+    lcd.print(midiVelocity);
+    lcd.setCursor(15, 1);
+    lcd.print(">");
+    break;
+  case MENU_PLAY:
+    lcd.print("SELECT to start");
+    break;
+  }
+}
+
+// Draw the play mode status screen
+void drawPlayScreen()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Playing...");
+  lcd.setCursor(0, 1);
+  lcd.print("Ch:");
+  if (midiChannel + 1 < 10)
+    lcd.print("0");
+  lcd.print(midiChannel + 1);
+  lcd.print(" Vel:");
+  lcd.print(midiVelocity);
+}
+
+// -------------------------
+// Mode loops
+// -------------------------
+
+void setupModeLoop()
+{
+  uint8_t buttons = lcd.readButtons();
+  uint8_t pressed = buttons & ~lastButtons; // rising edge only
+  lastButtons = buttons;
+
+  if (!pressed && !lcdDirty)
+    return;
+
+  if (pressed & BUTTON_UP)
+  {
+    if (menuIndex > 0)
+      menuIndex--;
+    lcdDirty = true;
+  }
+  if (pressed & BUTTON_DOWN)
+  {
+    if (menuIndex < MENU_COUNT - 1)
+      menuIndex++;
+    lcdDirty = true;
+  }
+  if (pressed & BUTTON_LEFT)
+  {
+    switch (menuIndex)
+    {
+    case MENU_STRING_MODE:
+      if (stringMode > 0)
+        stringMode--;
+      stringNotes = stringModeNotes[stringMode];
+      break;
+    case MENU_MIDI_CHANNEL:
+      if (midiChannel > 0)
+        midiChannel--;
+      break;
+    case MENU_MIDI_VELOCITY:
+      if (midiVelocity > 1)
+        midiVelocity--;
+      break;
+    }
+    lcdDirty = true;
+  }
+  if (pressed & BUTTON_RIGHT)
+  {
+    switch (menuIndex)
+    {
+    case MENU_STRING_MODE:
+      if (stringMode < STRING_MODE_COUNT - 1)
+        stringMode++;
+      stringNotes = stringModeNotes[stringMode];
+      break;
+    case MENU_MIDI_CHANNEL:
+      if (midiChannel < 15)
+        midiChannel++;
+      break;
+    case MENU_MIDI_VELOCITY:
+      if (midiVelocity < 127)
+        midiVelocity++;
+      break;
+    }
+    lcdDirty = true;
+  }
+  if (pressed & BUTTON_SELECT)
+  {
+    harpMode = HARP_MODE_PLAY;
+    lcdDirty = true;
+    lcd.setBacklight(LCD_BACKLIGHT_RED);
+    drawPlayScreen();
+    Serial.println("Entering play mode");
+    return;
+  }
+
+  if (lcdDirty)
+  {
+    drawMenu();
+    lcdDirty = false;
+  }
+}
+
+void playModeLoop()
+{
+  // TODO: sensor scanning, debounce, edge detection, MIDI note emission
+
+  uint8_t buttons = lcd.readButtons();
+  uint8_t pressed = buttons & ~lastButtons;
+  lastButtons = buttons;
+
+  if (pressed & BUTTON_SELECT)
+  {
+    harpMode = HARP_MODE_SETUP;
+    lcdDirty = true;
+    lcd.setBacklight(LCD_BACKLIGHT_GREEN);
+    Serial.println("Entering setup mode");
+  }
+}
+
+// -------------------------
+// Setup & main loop
+// -------------------------
+
 void setup()
 {
   Serial.begin(9600);       // Debug serial
@@ -68,23 +245,16 @@ void setup()
   }
 
   lcd.begin(16, 2);
-  lcd.setBacklight(LCD_BACKLIGHT_GREEN); // Start with backlight off
+  lcd.setBacklight(LCD_BACKLIGHT_GREEN);
+  drawMenu();
+  lcdDirty = false;
   Serial.println("Harp started");
 }
 
 void loop()
 {
-  /**
-   * If in setup mode, see a menu structure on the LCD.
-   * Navigate with up and down arrows, change values with left and right arrows, and select with the "Select" button.
-   * Menu options:
-   * - String mode: Tom or Ox7 (or more in the future)
-   * - MIDI channel: 1-16
-   * - MIDI velocity: 1-127
-   * - Switch to play mode
-   *
-   *
-   * If in play mode, read the sensors and send MIDI notes accordingly.
-   * Pressing the "Select" button switches back to setup mode to allow changing the string note mapping.
-   */
+  if (harpMode == HARP_MODE_SETUP)
+    setupModeLoop();
+  else
+    playModeLoop();
 }
--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Permissions get reset if you unplug the board
-sudo chmod  a+rw /dev/ttyACM0
+sudo chmod a+rw /dev/ttyACM0
 # Compile and upload the sketch
 arduino-cli compile --fqbn arduino:avr:mega $1
 arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega $1
